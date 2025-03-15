31cfc831df5d9a10523b57916594986be8d3276c
noelregis18 <noel.regis04@gmail.com>
2025-03-15 14:49:50 +0530

    Add files via upload

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,10 @@
-import { getJestProjects } from "@nx/jest";
-
 export default {
-  projects: getJestProjects(),
+  displayName: "server",
+  preset: "../../jest.preset.js",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
+  },
+  moduleFileExtensions: ["ts", "js", "html"],
+  coverageDirectory: "../../coverage/apps/server",
 };
